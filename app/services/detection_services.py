7252fc9d242572939b38e37f69e437208f85fda9
Nyxteridass <cb.varadero@gmail.com>
2026-03-19 17:31:02 +0200

    logger

--- app/services/detection_services.py
+++ app/services/detection_services.py
@@ -2,12 +2,12 @@ import re
 from app.database.connection import DBConnectionManager
 from app.database.dbmodels import StolenVehicle
 
-
+#region Comments
 # Υπηρεσία που ελέγχει την πινακίδα ως προς την ορθότητα για τον περιορισμό των αχρείαστων queries
 # προς τη βάση και μετά φέρνει την "κατάσταση" του οχήματος πάλι προς το UI. Εάν ΔΕΝ εντοπιστεί τέτοια 
 # πινακίδα, επιστρέφει status "CLEAN" και default χρώμα πράσινο. Εάν εντοπιστεί, επιστρέφει το status 
 # του οχήματος στη βάση μας (STOLEN/WANTED) και το αντίστοιχο χρώμα για να περάσει ως alert στο UI.
-
+#endregion
 class DetectionService:
     def __init__(self):
         self.session_factory = DBConnectionManager.get_session
@@ -15,12 +15,13 @@ class DetectionService:
     def process_detected_plate(self, raw_text):
         # "Καθαρισμός" του κειμένου: Αφαιρούμε όλα τα μη-αλφαριθμητικά και μετατρέπουμε σε κεφαλαία
         clean_text = "".join(re.findall(r'[A-Z0-9]', raw_text.upper()))
-        
-        # Regex για ελληνικό format (3 γράμματα που συνοδεύονται από 1- 4 αριθμούς , για να συμπεριλάβουμε και 
-        # πινακίδες δικύκλων που έχουν 3 γράμματα αλλά από 1 έως 3 αριθμούς). Με αλλαγή στο regex μπορούμε να 
-        # συμπεριλάβουμε και πινακίδες με 2 γράμματα (π.χ. για παλιά οχήματα) ή να επεκτείνουμε την αναζήτηση 
+#region  process_detected_plate() ---Comments 
+        # Regex για ελληνικό format (3 γράμματα που συνοδεύονται από 1- 4 αριθμούς , 
+        # για να συμπεριλάβουμε και  πινακίδες δικύκλων που έχουν 3 γράμματα αλλά 
+        # από 1 έως 3 αριθμούς). Με αλλαγή στο regex μπορούμε να συμπεριλάβουμε και 
+        # πινακίδες με 2 γράμματα (π.χ. για παλιά οχήματα) ή να επεκτείνουμε την αναζήτηση 
         # σε πινακίδες άλλων τύπων οχημάτων (πχ φορτηγά).
-        
+#endregion       
         pattern = r'^[ABEZHIKMNOPTYX]{3}\d{1,4}$'
         if not re.match(pattern, clean_text):
             return None # Δεν είναι έγκυρη ελληνική πινακίδα
--- app/services/logger.py
+++ app/services/logger.py
@@ -1,11 +1,10 @@
 import logging
-from pathlib import Path
-from app.utils.pathfinder import BASE_DIR
+from app.utils.pathfinder import LOGS_DIR_PATH
 
 #Εισάγουμε το BASE_DIR και πριν την δημιουργία του logger ελέγχουμε την ύπαρξη 
 # του φακέλου με τα logs.Στην περίπτωση που δεν υπάρχει - δημιουργείται εδώ.
-LOGS_DIR = Path(BASE_DIR) / "logs"
-LOGS_DIR.mkdir(exist_ok=True)
+
+LOGS_DIR_PATH.mkdir(exist_ok=True)
 
 def _setup_logger(name: str, log_file: str) -> logging.Logger:
     #Δημιουργούμε ένα logger setup (generic).
@@ -18,12 +17,10 @@ def _setup_logger(name: str, log_file: str) -> logging.Logger:
         #  και μετά το μήνυμα που δέχεται στο αντίστοιχο αρχείο 
         formatter = logging.Formatter('%(asctime)s, %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         
-        file_handler = logging.FileHandler(LOGS_DIR / log_file, encoding='utf-8')
+        file_handler = logging.FileHandler(LOGS_DIR_PATH / log_file, encoding='utf-8')
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-        
-        
-        
+                
     return logger
 
 # Αρχικοποίηση των 3 loggers όταν φορτώνεται το αρχείο
@@ -34,28 +31,28 @@ _err_logger = _setup_logger("errors", "errors.log")
 # Συναρτήσεις για τους διάφορους τύπους καταγραφών:
 
 def log_detection(plate: str, status: str, confidence: float, ocr_model: str):
+    #region Comments
     # Γράφει στο αρχείο detections.log : timestamp, πινακίδα που ανιχνεύθηκε, status ,
     #  confidence του μοντέλου που χρησιμοποιήθηκε και ποιο μοντέλο OCR έχει χρησιμοποιηθεί
     # στη συγκεκριμένη ανίχνευση για λόγους στατιστικής ανάλυσης.
-    
+    #endregion
     # Μορφοποίηση του confidence σε 2 δεκαδικά (π.χ. 0.95)
     _det_logger.info(f"{plate}, {status}, {confidence:.2f}, {ocr_model}")
 
 def log_added_vehicle(plate: str, status: str):
-    # Γράφει στο αρχείο access.log , την ΠΡΟΣΘΗΚΗ οχήματος στη βάση.
-    
+    # Γράφει στο αρχείο access.log , την ΠΡΟΣΘΗΚΗ οχήματος στη βάση.   
     _acc_logger.info(f"{plate}, {status}, ADDED_TO_DB ")
 
 def log_access(username: str, role: str, action: str):
     # Γράφει στο access.log, ΚΑΘΕ ενέργεια που σχετίζεται με Χρήστες / απόπειρες εισόδου στο σύστημα 
-
     _acc_logger.info(f"{username}, {role}, {action}")
 
 def log_error(error_type: str, file_name: str, details: str = ""):
+    #region
     # Γράφει στο errors.log, κάθε σφάλμα που προκύπτει κατά τη λειτουργία του συστήματος.
     # Εκτός από το timestamp  δίνει τον τύπο σφάλματος και το αρχείο που το προκάλεσε και μπορεί 
     # να συμπεριλάβει και λεπτομέρειες.
-   
+    #endregion
     msg = f"{error_type}, {file_name}"
     if details:
         msg += f", {details}"
